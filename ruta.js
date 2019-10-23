9d71ec1d74a9cb1fbe01e0d2e01fedc63ad81a35
Kaluz27 <eluna3@ucol.mx>
2019-10-23 16:13:47 -0500

    Se me habia olvidado hacer push

---
+++ ruta.js
@@ -0,0 +1,31 @@
+export default class Ruta{
+    constructor(ruta){
+        this._numero = ruta.numero;
+        this._matricula = ruta.matricula;
+        this._siguiente = null;
+        this._anterior = null;
+    }
+    get numero(){
+        return this._numero;
+    }
+    get matricula(){
+        return this._matricula;
+    }
+    get siguiente(){
+        return this._siguiente;
+    }
+    get anterior(){
+        return this._anterior;
+    }
+    set siguiente(siguiente){
+        this._siguiente = siguiente;
+    }
+    set anterior(anterior){
+        this._anterior = anterior;
+    }
+
+    toString(){
+        let s = `La ruta ${this._numero} tiene la matrícula ${this._matricula}`;
+        return s
+    }
+}
